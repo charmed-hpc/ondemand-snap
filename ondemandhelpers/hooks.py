90b748af3bd234082c35484ce18da087854320d8
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2024-02-27 17:07:16 -0500

    feat: Push current progress on snap package
    Apache is working now, but additional configuration is required to get the portal generator functional.
    
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- ondemandhelpers/hooks.py
+++ ondemandhelpers/hooks.py
@@ -13,3 +13,54 @@
 # limitations under the License.
 
 """Hooks for the Ondemand snap."""
+
+import logging
+from pathlib import Path
+
+from snaphelpers import Snap
+
+from .log import setup_logging
+
+
+def _setup_dirs(snap: Snap) -> None:
+    """Create directories required by Ondemand, Nginx, and Passenger.
+
+    Args:
+        snap: The Snap instance.
+    """
+    logging.info("Provisioning required directories for Ondemand, Nginx, and Passenger.")
+    run = Path(snap.paths.common) / "run"
+    etc = Path(snap.paths.common) / "etc"
+    var = Path(snap.paths.common) / "var"
+    for directory in [
+        # etc - configuration files.
+        etc / "apache2" / "conf.d",
+        etc / "ood" / "config" / "clusters.d",
+        etc / "ood" / "config" / "ondemand.d",
+        # run - runtime variable data.
+        run,
+        # var/lib - variable state information.
+        var / "lib" / "nginx" / "config" / "puns",
+        var / "lib" / "nginx" / "config" / "app" / "sys",
+        var / "lib" / "nginx" / "config" / "app" / "usr",
+        var / "lib" / "nginx" / "config" / "app" / "dev",
+        # var/log - variable log data.
+        var / "log" / "apache",
+        # var/www - variable web data.
+        var / "www" / "ood" / "public" / "maintenance",
+        var / "www" / "ood" / "discover",
+        var / "www" / "ood" / "register",
+        var / "www" / "ood" / "apps" / "sys",
+        var / "www" / "ood" / "apps" / "usr",
+        var / "www" / "ood" / "apps" / "dev",
+    ]:
+        logging.debug("Generating directory %s.", directory)
+        directory.mkdir(parents=True)
+
+
+def install(snap: Snap) -> None:
+    """Install hook for the Ondemand snap."""
+    setup_logging(snap.paths.common / "hooks.log")
+
+    logging.info("Executing snap `install` hook.")
+    _setup_dirs(snap)
